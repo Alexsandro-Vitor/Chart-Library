55f8610c8957132f1db25ab6d559290193244137
Alexsandro Vítor <avsc@cin.ufpe.br>
2018-05-19 21:38:34 -0300

    Mudança no posicionamento dos gráficos

--- Chart.js
+++ Chart.js
@@ -1,36 +1,56 @@
 /**
- * Class that represents a Chart
+ * Class that represents a Chart.
  */
  class Chart {
 	/**
 	 * @constructor
-	 * @param {Object} container - The tag in which the chart will be inserted
-	 * @param {string} id - The id of the chart tag
-	 * @param {(Object|number)} margins - The margins of the chart. If a number is passed, all its values will be the same
-	 * @param {number} margins.left - Left margin of the chart
-	 * @param {number} margins.right - Right margin of the chart
-	 * @param {number} margins.top - Upper margin of the chart
-	 * @param {number} margins.bottom - Lower margin of the chart
-	 * @param {number} totalWidth - The width of the chart, counting the margins
-	 * @param {number} totalHeight - The height of the chart, counting the margins
-	 * @param {string} tagClass - The type of chart 
+	 * @param {d3.selection} container - The tag in which the chart will be inserted.
+	 * @param {string} id - The id of the chart tag.
+	 * @param {Object} position - The position of the chart.
+	 * @param {number} position.x - The X coordinate of the chart.
+	 * @param {number} position.y - The Y coordinate of the chart.
+	 * @param {(number|Object)} margins - The margins of the chart. If a number is passed, all its values will be the same.
+	 * @param {number} margins.left - Left margin of the chart.
+	 * @param {number} margins.right - Right margin of the chart.
+	 * @param {number} margins.top - Upper margin of the chart.
+	 * @param {number} margins.bottom - Lower margin of the chart.
+	 * @param {Object} dimensions - The dimensions of the chart.
+	 * @param {number} dimensions.width - The width of the chart, counting the margins.
+	 * @param {number} dimensions.height - The height of the chart, counting the margins.
+	 * @param {string} tagClass - The type of chart.
 	 */
-	constructor(container, id, margins, totalWidth, totalHeight, tagClass) {
+	constructor(container, id, position, margins, dimensions, tagClass) {
 		/**
-		 * The tag in which the chart will be inserted
-		 * @member {Object} container
+		 * The tag in which the chart will be inserted.
+		 * @member {d3.selection} Chart#container
 		 */
 		this.container = container;
 		
 		/**
-		 * The id of the chart tag
-		 * @member {string} id
+		 * The id of the chart tag.
+		 * @member {string} Chart#id
 		 */
 		this.id = id;
 		
 		/**
-		 * The margins of the chart
-		 * @member {Object} margins
+		 * The X coordinate of the chart.
+		 * @member {number} Chart#x
+		 */
+		/**
+		 * The Y coordinate of the chart.
+		 * @member {number} Chart#y
+		 */
+		if ((position == null) || (typeof(position) != "object")) {
+			this.x = 0;
+			this.y = 0;
+		} else {
+			this.x = position.x;
+			this.y = position.y;
+		}
+		
+		/**
+		 * The margins of the chart.
+		 * @member {Object} Chart#margins
 		 */
 		if (margins == null) {
 			this.margins = {left:10, right:10, top:10, bottom:10};
@@ -39,21 +59,29 @@
 		} else {
 			this.margins = margins;
 		}
+		this.margins.left += this.x;
+		this.margins.top += this.y;
 		
 		/**
-		 * The inside width of the margin
-		 * @member {number} width
+		 * The inside width of the margin.
+		 * @member {number} Chart#width
 		 */
-		this.width = totalWidth - this.margins.left - this.margins.right;
 		/**
-		 * The inside height of the margin
-		 * @member {number} height
+		 * The inside height of the margin.
+		 * @member {number} Chart#height
 		 */
-		this.height = totalHeight - this.margins.top - this.margins.bottom;
+		if (dimensions == null) {
+			this.width = container.attr("width") - this.margins.left - this.margins.right + this.x;
+			this.height = container.attr("height") - this.margins.top - this.margins.bottom + this.y;
+		} else {
+			this.width = dimensions.width - this.margins.left - this.margins.right;
+			this.height = dimensions.height - this.margins.top - this.margins.bottom;
+		}
 		
 		/**
-		 * The tag of the chart
-		 * @member {Object} tag
+		 * The selection of the chart.
+		 * @member {d3.selection} Chart#tag
+		 * @default d3.selectAll("." + tagClass).select("#" + this.id)
 		 */
 		this.tag = this.container.append("g")
 			.attr("id", this.id)
@@ -62,11 +90,11 @@
 	}
 	
 	/** 
-	 * Generates an array with equally distanced values
-	 * @param {number} start - The first value of the returned array
-	 * @param {number} size - The size of the returned array
-	 * @param {number} end - THe last value of the returned array
-	 * @returns {number[]} Array with equally distanced values
+	 * Generates an array with equally distanced values.
+	 * @param {number} start - The first value of the returned array.
+	 * @param {number} size - The size of the returned array.
+	 * @param {number} end - The last value of the returned array.
+	 * @returns {number[]} Array with equally distanced values.
 	 */
 	static genSequence(start, size, end) {
 		var output = [];
@@ -78,12 +106,12 @@
 	}
 	
 	/**
-	 * Adjusts the domain of a scale and its axis
-	 * @param {Object} scale - The scale to be adjusted
-	 * @param {Obejct} axis - The axis which uses the scale
-	 * @param {Object} axisGroup - The group in which the axis is
-	 * @param {number} minValue - The minimum value of the new domain
-	 * @param {number} maxValue - The maximum value of the new domain
+	 * Adjusts the domain of a scale and its axis.
+	 * @param {d3.scale} scale - The scale to be adjusted.
+	 * @param {d3.axis} axis - The axis which uses the scale.
+	 * @param {d3.selection} axisGroup - The group in which the axis is.
+	 * @param {number} minValue - The minimum value of the new domain.
+	 * @param {number} maxValue - The maximum value of the new domain.
 	 */
 	static adjustScaleDomain(scale, axis, axisGroup, minValue, maxValue) {
 		scale.domain([minValue, maxValue]);
@@ -92,20 +120,20 @@
 	}
 	
 	/** 
-	 * Adds a value to a field if it's null
-	 * @param {Object} array - The array which will have a value added
-	 * @param {(string|number)} field - The name of the field
-	 * @param {(function|number|string)} value - The value added
+	 * Adds a value to a field if it's null.
+	 * @param {Object} array - The array which will have a value added.
+	 * @param {(number|string)} field - The name of the field.
+	 * @param {(function|number|string)} value - The value added.
 	 */
 	static addIfNull(array, field, value) {
 		if (array[field] == null) array[field] = value;
 	}
 	
 	/**
-	 * Sets attributes and events of a selection
-	 * @param {selection} selection - The selection of elements used
-	 * @param {Object} attributes - An object containing functions or constants for attributes of the selected elements
-	 * @param {Object} onEvents - An object containing functions for events
+	 * Sets attributes and events of a selection.
+	 * @param {d3.selection} selection - The selection of elements used.
+	 * @param {Object} attributes - An object containing functions or constants for attributes of the selected elements.
+	 * @param {Object} onEvents - An object containing functions for events.
 	 */
 	static insertAttributesEvents(selection, attributes, onEvents) {
 		//Setting attributes
