56d863e60abe9028bd10f7dc746c70254d2f23d3
Alexsandro Vítor <avsc@cin.ufpe.br>
2018-06-02 22:06:52 -0300

    Adição de funções de get/set em Segments.js

--- Segments.js
+++ Segments.js
@@ -22,138 +22,164 @@ class Segments extends Chart {
 	constructor(container, id, position, margins, dimensions) {
 		super(container, id, position, margins, dimensions, "segmentsChart");
 		
-		/**
-		 * The X scale of the chart. Used by the segments.
-		 * @member {d3.scale} Segments#xScale
-		 * @default d3.scaleLinear()
-		 */
-		this.xScale = d3.scaleLinear();
-		
-		/**
-		 * The X scale of the chart. Used by the axis.
-		 * @member {d3.scale} Segments#xAxisScale
-		 * @default d3.scaleOrdinal().range([0, this.width()])
-		 */
-		this.xAxisScale = d3.scaleOrdinal()
+		this._xScale = d3.scaleLinear()
 			.range([0, this._width]);
-		/**
-		 * The X axis of the chart.
-		 * @member {d3.axis} Segments#xAxis
-		 * @default d3.axisBottom(this.xAxisScale)
-		 */
-		this.xAxis = d3.axisBottom(this.xAxisScale);
-		/**
-		 * The group of the X axis.
-		 * @member {d3.selection} Segments#xAxisGroup
-		 */
-		this.xAxisGroup = this._selection
+		
+		this._xAxisScale = d3.scaleOrdinal()
+			.range([0, this._width]);
+		this._xAxis = d3.axisBottom(this._xAxisScale);
+		this._xAxisGroup = this._selection
 			.append("g")
 			.attr("class", "xAxis")
 			.attr("transform", "translate(0," + this._height  + ")");
-		this.xAxisGroup.call(this.xAxis);
+		this._xAxisGroup.call(this._xAxis);
 		
-		/**
-		 * The Y scale of the chart. Used by the axis and the columns.
-		 * @member {d3.scale} Segments#yScale
-		 * @default d3.scaleLinear().range([this.height(), 0])
-		 */
-		this.yScale = d3.scaleLinear()
+		this._yScale = d3.scaleLinear()
 			.range([this._height, 0]);
-		/**
-		 * The Y axis of the chart.
-		 * @member {d3.axis} Segments#yAxis
-		 * @default d3.axisLeft(this.yScale)
-		 */
-		this.yAxis = d3.axisLeft(this.yScale);
-		/**
-		 * The group of the Y axis.
-		 * @member {d3.selection} Segments#yAxisGroup
-		 */
-		this.yAxisGroup = this._selection
+		this._yAxis = d3.axisLeft(this._yScale);
+		this._yAxisGroup = this._selection
 			.append("g")
 			.attr("class","yAxis");
-		this.yAxisGroup.call(this.yAxis);
+		this._yAxisGroup.call(this._yAxis);
 		
-		/**
-		 * The names in the X axis.
-		 * @member {string[]} Segments#xAxisNames
-		 */
-		this.xAxisNames = [];
-		
+		//Layers to make sure the ranges stay behind the segments and the segments stay behind the dots
 		this._rangeLayer = this._selection.append("g");
 		this._segLayer = this._selection.append("g");
 		this._dotLayer = this._selection.append("g");
 		
-		/**
-		 * The segments of the chart.
-		 * @member {d3.selection} Segments#segSelection
-		 */
-		this.segSelection = null;
-		/**
-		 * The dots of the chart.
-		 * @member {d3.selection} Segments#dotSelection
-		 */
-		this.dotSelection = null;
-		/**
-		 * The ranges of the chart.
-		 * @member {d3.selection} Segments#rangeSelection
-		 */
-		this.rangeSelection = null;
+		this._segSelection = null;
 		
-		/**
-		 * A path generator for the segments.
-		 * @member {d3.line} Segments#segPathGenerator
-		 * @default d3.line().x((d, i)=>this.xScale(i)).y((d, i)=>this.yScale(d));
-		 */
+		this._dotSelection = null;
+		
+		this._rangeSelection = null;
+		
 		this.segPathGenerator = d3.line()
-			.x((d, i)=>this.xScale(i))
-			.y((d, i)=>this.yScale(d));
+			.x((d, i)=>this._xScale(i))
+			.y((d, i)=>this._yScale(d));
 		
-		/**
-		 * A path generator for the range.
-		 * @member {d3.area} Segments#rangePathGenerator
-		 * @default d3.area().x((d, i)=>this.xScale(i)).y0((d, i)=>this.yScale(d[0])).y1((d, i)=>this.yScale(d[1]));
-		 */
 		this.rangePathGenerator = d3.area()
-			.x((d, i)=>this.xScale(i))
-			.y0((d, i)=>this.yScale(d[0]))
-			.y1((d, i)=>this.yScale(d[1]));
+			.x((d, i)=>this._xScale(i))
+			.y0((d, i)=>this._yScale(d[0]))
+			.y1((d, i)=>this._yScale(d[1]));
 		
-		/**
-		 * The color scale for dots on the chart. Used to set the colors of each dotGroup in the chart.
-		 * @member {d3.scale} Segments#dotColorScale
-		 * @default d3.scaleLinear().domain(Chart.genSequence(0, d3.schemeSet1.length, d3.schemeSet1.length - 1)).range(d3.schemeSet1)
-		 */
-		this.dotColorScale = d3.scaleLinear()
+		this._dotColorScale = d3.scaleLinear()
 			.domain(Chart.genSequence(0, d3.schemeSet1.length, d3.schemeSet1.length - 1))
 			.range(d3.schemeSet1);
 		
-		/**
-		 * The color scale for ranges on the chart. Used to set the colors of each scale in the chart.
-		 * @member {d3.scale} Segments#rangeColorScale
-		 * @default d3.scaleLinear().domain(Chart.genSequence(0, d3.schemeSet2.length, d3.schemeSet2.length - 1)).range(d3.schemeSet2)
-		 */
-		this.rangeColorScale = d3.scaleLinear()
+		this._rangeColorScale = d3.scaleLinear()
 			.domain(Chart.genSequence(0, d3.schemeSet2.length, d3.schemeSet2.length - 1))
 			.range(d3.schemeSet2);
 	}
 	
-	/** 
-	 * Sets the names the X axis.
-	 * @param {string[]} newDomain - An array of names for the X axis.
+	/**
+	 * The X scale of the axis. If scale is given, sets it and also sets the X axis, otherwise returns the current xAxisScale.
+	 * @param {d3.scale} scale - The new xAxisScale.
+	 * @returns {(Segments|d3.scale)} This object or the current xAxisScale.
 	 */
-	setXDomain(newDomain) {
-		this.xAxisNames = newDomain.slice();
-		let sequence = Chart.genSequence(0, newDomain.length, this._width);
-		this.xAxisScale
-			.domain(newDomain)
-			.range(sequence);
-		this.xAxis.scale(this.xAxisScale);
-		this.xAxisGroup.call(this.xAxis);
-		
-		this.xScale
-			.domain([0, this.xAxisNames.length-1])
-			.range([0, this._width]);
+	xAxisScale(scale) {
+		if (scale) {
+			this._xAxisScale = scale;
+			this._xScale.domain([0, this._xAxisScale.domain().length-1]);
+			Chart.adjustScaleDomain(this._xAxisScale, this._xAxis, this._xAxisGroup, this._xAxisScale.domain());
+			return this;
+		} else {
+			return this._xScale;
+		}
+	}
+	
+	/**
+	 * The Y scale of the chart. If scale is given, sets it and also sets the Y axis, otherwise returns the current yScale.
+	 * @param {d3.scale} scale - The new yScale.
+	 * @returns {(Segments|d3.scale)} This object or the current yScale.
+	 */
+	yScale(scale) {
+		if (scale) {
+			this._yScale = scale;
+			Chart.adjustScaleDomain(this._yScale, this._yAxis, this._yAxisGroup, d3.extent(this._yScale.domain()));
+			return this;
+		} else {
+			return this._yScale;
+		}
+	}
+	
+	/**
+	 * Returns the selection of the segments of the chart.
+	 * @returns {d3.selection} The segments of this chart.
+	 */
+	segSelection() {
+		return this._segSelection;
+	}
+	
+	/**
+	 * Returns the selection of the dots of the chart.
+	 * @returns {d3.selection} The dots of this chart.
+	 */
+	dotSelection() {
+		return this._dotSelection;
+	}
+	
+	/**
+	 * Returns the selection of the ranges of the chart.
+	 * @returns {d3.selection} The ranges of this chart.
+	 */
+	rangeSelection() {
+		return this._rangeSelection;
+	}
+	
+	/**
+	 * A path generator for the segments. If gen is given, sets it, otherwise returns the current segPathGenerator.
+	 * @param {d3.line} gen - The new segPathGenerator.
+	 * @returns {(Segments|d3.line)} This object or the current segPathGenerator.
+	 */
+	segPathGenerator(gen) {
+		if (gen) {
+			this._segPathGenerator = gen;
+			return this;
+		} else {
+			return this._segPathGenerator;
+		}
+	}
+	
+	/**
+	 * A path generator for the range. If gen is given, sets it, otherwise returns the current rangePathGenerator.
+	 * @param {d3.area} gen - The new rangePathGenerator.
+	 * @returns {(Segments|d3.area)} This object or the current rangePathGenerator.
+	 */
+	rangePathGeneration(gen) {
+		if (gen) {
+			this._rangePathGenerator = gen;
+			return this;
+		} else {
+			return this._rangePathGenerator;
+		}
+	}
+	
+	/**
+	 * The color scale for ranges on the chart. Used to set the colors of each scale in the chart. If scale is given, sets it, otherwise returns the current dotColorScale.
+	 * @param {d3.scale} scale - The new dotColorScale.
+	 * @returns {(Segments|d3.scale)} This object or the current dotColorScale.
+	 */
+	dotColorScale(scale) {
+		if (scale) {
+			this._dotColorScale = scale;
+			return this;
+		} else {
+			return this._dotColorScale;
+		}
+	}
+	
+	/**
+	 * The color scale for ranges on the chart. Used to set the colors of each scale in the chart. If scale is given, sets it, otherwise returns the current rangeColorScale.
+	 * @param {d3.scale} scale - The new rangeColorScale.
+	 * @returns {(Segments|d3.scale)} This object or the current rangeColorScale.
+	 */
+	rangeColorScale(scale) {
+		if (scale) {
+			this._rangeColorScale = scale;
+			return this;
+		} else {
+			return this._rangeColorScale;
+		}
 	}
 	
 	/** 
@@ -167,9 +193,9 @@ class Segments extends Chart {
 		
 		//Adjusting the yScale and axis
 		let datasetExtent = dataset.map(d=>d3.extent(d));
-		datasetExtent.push(this.yScale.domain());
+		datasetExtent.push(this._yScale.domain());
 		let newDomain = [d3.min(datasetExtent.map(d=>d[0])), d3.max(datasetExtent.map(d=>d[1]))];
-		Chart.adjustScaleDomain(this.yScale, this.yAxis, this.yAxisGroup, newDomain);
+		Chart.adjustScaleDomain(this._yScale, this._yAxis, this._yAxisGroup, newDomain);
 		
 		//Mandatory attributes
 		if (attributes == null) attributes = [];
@@ -178,19 +204,19 @@ class Segments extends Chart {
 		Chart.addIfNull(attributes, "d", (d, i)=>(thisChart.segPathGenerator(d)));
 		Chart.addIfNull(attributes, "stroke", "black");
 		
-		this.segSelection = this._segLayer.selectAll(".segment").data(dataset).enter().append("path")
+		this._segSelection = this._segLayer.selectAll(".segment").data(dataset).enter().append("path")
 			.attr("fill", "transparent");
 		
 		//Updating previous selections
-		if (this.dotSelection) {
-			this.dotSelection
-				.attr("cx", (d, i)=>thisChart.xScale(i))
-				.attr("cy", (d, i)=>thisChart.yScale(d));
+		if (this._dotSelection) {
+			this._dotSelection
+				.attr("cx", (d, i)=>thisChart._xScale(i))
+				.attr("cy", (d, i)=>thisChart._yScale(d));
 		}
-		if (this.rangeSelection) this.rangeSelection.attr("d", (d, i)=>(thisChart.rangePathGenerator(d)));
+		if (this._rangeSelection) this._rangeSelection.attr("d", (d, i)=>(thisChart.rangePathGenerator(d)));
 		
 		//Insertion of attributes and events
-		Chart.insertAttributesEvents(this.segSelection, attributes, onEvents);
+		Chart.insertAttributesEvents(this._segSelection, attributes, onEvents);
 		
 		return this;
 	}
@@ -206,34 +232,34 @@ class Segments extends Chart {
 		
 		//Adjusting the yScale and axis
 		let datasetExtent = dataset.map(d=>d3.extent(d));
-		datasetExtent.push(this.yScale.domain());
+		datasetExtent.push(this._yScale.domain());
 		let newDomain = [d3.min(datasetExtent.map(d=>d[0])), d3.max(datasetExtent.map(d=>d[1]))];
-		Chart.adjustScaleDomain(this.yScale, this.yAxis, this.yAxisGroup, newDomain);
+		Chart.adjustScaleDomain(this._yScale, this._yAxis, this._yAxisGroup, newDomain);
 		
 		//Mandatory attributes
 		if (attributes == null) attributes = [];
 		Chart.addIfNull(attributes, "id", (d, i)=>("dotGroup" + i));
 		attributes["class"] = "dotGroup";
 		Chart.addIfNull(attributes, "r", "5px");
-		Chart.addIfNull(attributes, "cx", (d, i)=>thisChart.xScale(i));
-		Chart.addIfNull(attributes, "cy", (d, i)=>thisChart.yScale(d));
+		Chart.addIfNull(attributes, "cx", (d, i)=>thisChart._xScale(i));
+		Chart.addIfNull(attributes, "cy", (d, i)=>thisChart._yScale(d));
 		
 		//Creating the groups
-		this.dotSelection = this._dotLayer.selectAll(".dotGroup").data(dataset).enter().append("g")
+		this._dotSelection = this._dotLayer.selectAll(".dotGroup").data(dataset).enter().append("g")
 			.attr("id", attributes["id"])
 			.attr("class", attributes["class"])
-			.attr("fill", (d, i)=>(thisChart.dotColorScale(i % thisChart.dotColorScale.domain().length)))
+			.attr("fill", (d, i)=>(thisChart._dotColorScale(i % thisChart._dotColorScale.domain().length)))
 			.selectAll(".groupDot").data(d=>d).enter().append("circle");
 		
 		//Updating previous selections
-		if (this.segSelection) this.segSelection.attr("d", (d, i)=>(thisChart.segPathGenerator(d)));
-		if (this.rangeSelection) this.rangeSelection.attr("d", (d, i)=>(thisChart.rangePathGenerator(d)));
+		if (this._segSelection) this._segSelection.attr("d", (d, i)=>(thisChart.segPathGenerator(d)));
+		if (this._rangeSelection) this._rangeSelection.attr("d", (d, i)=>(thisChart.rangePathGenerator(d)));
 		
-		attributes["id"] = (d, i)=>("dot_" + thisChart.xAxisNames[i]);
+		attributes["id"] = (d, i)=>("dot_" + thisChart._xAxisScale.domain()[i]);
 		attributes["class"] = "groupDot";
 		
 		//Insertion of attributes and events
-		Chart.insertAttributesEvents(this.dotSelection, attributes, onEvents);
+		Chart.insertAttributesEvents(this._dotSelection, attributes, onEvents);
 		
 		return this;
 	}
@@ -251,9 +277,9 @@ class Segments extends Chart {
 		
 		//Adjusting the yScale and axis
 		let datasetExtent = dataset.map(d=>[d3.min(d.map(d=>d[0])), d3.max(d.map(d=>d[1]))]);
-		datasetExtent.push(this.yScale.domain());
+		datasetExtent.push(this._yScale.domain());
 		let newDomain = [d3.min(datasetExtent.map(d=>d[0])), d3.max(datasetExtent.map(d=>d[1]))];
-		Chart.adjustScaleDomain(this.yScale, this.yAxis, this.yAxisGroup, newDomain);
+		Chart.adjustScaleDomain(this._yScale, this._yAxis, this._yAxisGroup, newDomain);
 		
 		//Mandatory attributes
 		if (attributes == null) attributes = [];
@@ -261,55 +287,39 @@ class Segments extends Chart {
 		attributes["class"] = "range";
 		Chart.addIfNull(attributes, "d", (d, i)=>(thisChart.rangePathGenerator(d)));
 		
-		this.rangeSelection = this._rangeLayer.selectAll(".range").data(dataset).enter().append("path")
-			.attr("fill", (d, i)=>(thisChart.rangeColorScale(i % thisChart.rangeColorScale.domain().length)));
+		this._rangeSelection = this._rangeLayer.selectAll(".range").data(dataset).enter().append("path")
+			.attr("fill", (d, i)=>(thisChart._rangeColorScale(i % thisChart._rangeColorScale.domain().length)));
 		
 		//Updating previous selections
-		if (this.dotSelection) {
-			this.dotSelection
-				.attr("cx", (d, i)=>thisChart.xScale(i))
-				.attr("cy", (d, i)=>thisChart.yScale(d));
+		if (this._dotSelection) {
+			this._dotSelection
+				.attr("cx", (d, i)=>thisChart._xScale(i))
+				.attr("cy", (d, i)=>thisChart._yScale(d));
 		}
-		if (this.segSelection) this.segSelection.attr("d", (d, i)=>(thisChart.segPathGenerator(d)));
+		if (this._segSelection) this._segSelection.attr("d", (d, i)=>(thisChart.segPathGenerator(d)));
 		
 		//Insertion of attributes and events
-		Chart.insertAttributesEvents(this.rangeSelection, attributes, onEvents);
+		Chart.insertAttributesEvents(this._rangeSelection, attributes, onEvents);
 		
 		return this;
 	}
 	
 	/** 
-	 * Sets the new colors of the dotGroups. The colors need to be set here because you cant have an "fill" field in an array, since it's the name of an array function.
-	 * @param {string[]} newColors - An array of colors for the dotColorScale to work with.
-	 */
-	setDotColorScale(newColors) {
-		this.dotColorScale.range(newColors);
-	}
-	
-	/** 
-	 * Sets the new colors of the chart. The colors need to be set here because you cant have an "fill" field in an array, since it's the name of an array function.
-	 * @param {string[]} newColors - An array of colors for the rangeColorScale to work with.
-	 */
-	setRangeColorScale(newColors) {
-		this.rangeColorScale.range(newColors);
-	}
-	
-	/** 
 	 * Clears the chart, removing all plottings.
-	 * @returns {Scatterplot} This chart.
+	 * @returns {Segments} This chart.
 	 */
 	clear() {
-		if (this.dotSelection) {
-			this.dotSelection.remove();
-			this.dotSelection = null;
+		if (this._dotSelection) {
+			this._dotSelection.remove();
+			this._dotSelection = null;
 		}
-		if (this.segSelection) {
-			this.segSelection.remove();
-			this.segSelection = null;
+		if (this._segSelection) {
+			this._segSelection.remove();
+			this._segSelection = null;
 		}
-		if (this.rangeSelection) {
-			this.rangeSelection.remove();
-			this.rangeSelection = null;
+		if (this._rangeSelection) {
+			this._rangeSelection.remove();
+			this._rangeSelection = null;
 		}
 		return super.clear();
 	}
